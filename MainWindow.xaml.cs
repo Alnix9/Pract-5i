0a42824c1435e730cb073966581bf32e6db11155
snarix_work <art.varlamov.04@mail.ru>
2022-12-01 16:39:02 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Генеральский_практическая_5
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void PerimetrButton(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(tbDoublingA.Text, out int _A) & !int.TryParse(tbDoublingB.Text,out int _B) & !int.TryParse(tbDoublingC.Text,out int _C))
+            {
+                MessageBox.Show("Установите параметры прежде чем узнавать их периметр" , "Вы нублан" , MessageBoxButton.OK,MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                 Triangle _triangle1 = new Triangle(_A,_B,_C);
+                 _triangle1.Perimetr(_A, _B, _C);
+                 tbPerimetr1.Text = Convert.ToString(_triangle1.A + _triangle1.B + _triangle1.C);
+            }
+        }
+        private void DoublingButton(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(tbSideA1.Text, out int _A) & !int.TryParse(tbSideB1.Text, out int _B) & !int.TryParse(tbSideC1.Text, out int _C))
+            {
+                MessageBox.Show("Установите параметры прежде чем узнавать удваивать их", "Вы нублан", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                Triangle _doubling = new Triangle(_A, _B, _C);
+                _doubling.Doubling(_A, _B, _C);
+                tbDoublingA.Text = Convert.ToString(_doubling.A);
+                tbDoublingB.Text = Convert.ToString(_doubling.B);
+                tbDoublingC.Text = Convert.ToString(_doubling.C);
+            }
+        }
+        private void SetParams1(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(tbSideA1.Text, out int _A) & !int.TryParse(tbSideB1.Text, out int _B) & !int.TryParse(tbSideC1.Text, out int _C))
+            {
+                MessageBox.Show("Установите параметры", "Вы нублан", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Triangle _setParams1 = new Triangle(_A, _B, _C);
+            _setParams1.SetParams(_A, _B, _C);
+            tbTriangle1.Text = Convert.ToString(_setParams1.A) + "," + Convert.ToString(_setParams1.B) + "," + Convert.ToString(_setParams1.C);
+        }
+        private void SetParams2(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(tbSideA2.Text, out int _A) & !int.TryParse(tbSideB2.Text, out int _B) & !int.TryParse(tbSideC2.Text, out int _C))
+            {
+                MessageBox.Show("Установите параметры", "Вы нублан", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            Triangle _setParams2 = new Triangle(_A, _B, _C);
+            _setParams2.SetParams(_A, _B, _C);
+            tbTriangle2.Text = Convert.ToString(_setParams2.A) + "," + Convert.ToString(_setParams2.B) + "," + Convert.ToString(_setParams2.C);
+        }
+        private void About_Program(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("5. Создать класс Triangle (треугольник) с полями-сторонами. " +
+                "Создать необходимые \r\nметоды и свойства. Определить метод вычисления периметра. " +
+                "Создать \r\nперегруженные методы SetParams, для установки параметров объекта, в том числе \r\nувеличения размеров треугольника в 2 раза.");
+        }
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
+
+   
+
---
+++ Triangle.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Генеральский_практическая_5
+{
+    internal class Triangle
+    {
+        private int _a;
+        private int _b;
+        private int _c;
+
+        public Triangle(int a, int b, int c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+        }
+        public int Perimetr(int a, int b, int c)
+        {
+            _a = a;
+            _b = b;
+            _c = c;
+            return a + b + c;
+        }
+        public int A
+        {
+            get => _a;
+            set
+            {
+                _a = value;
+            }
+        }
+        public int B
+        {
+            get => _b;
+            set
+            {
+                _b = value;
+            }
+        }
+       public int C
+        {
+            get => _c;
+            set
+            {
+                _c = value;
+            }
+        }
+        public void SetParams(int value)
+        {
+            A = value;
+            B = value;
+            C = value;
+        }
+        public void SetParams(int AValue,int BValue,int CValue)
+        {
+            A = AValue;
+            B = BValue;
+            C = CValue;
+        }
+        public void Doubling(int DoublingA,int DoublingB,int DoublingC)
+        {
+            A = DoublingA;
+            B = DoublingB;
+            C = DoublingC;
+            A *= 2;
+            B *= 2;   
+            C *= 2;
+        }
+    }
+}
